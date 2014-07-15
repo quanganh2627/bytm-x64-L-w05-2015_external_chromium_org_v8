d4205b7f94e015214bb2fdd95bb32342c5b62f4b
Bo Liu <boliu@google.com>
2014-07-15 14:01:39 -0700

    Update makefiles after merge of Chromium at 37.0.2062.21
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I7a0c35d35e9af8d2978d9e9b7e055a9fc89dfe49

--- tools/gyp/v8_nosnapshot.host.darwin-arm64.mk
+++ tools/gyp/v8_nosnapshot.host.darwin-arm64.mk
@@ -56,7 +56,6 @@ MY_CFLAGS_Debug := \
 	-m64 \
 	-Os \
 	-g \
-	-fomit-frame-pointer \
 	-fdata-sections \
 	-ffunction-sections \
 	-funwind-tables
@@ -131,7 +130,6 @@ MY_CFLAGS_Release := \
 	-fno-ident \
 	-fdata-sections \
 	-ffunction-sections \
-	-fomit-frame-pointer \
 	-funwind-tables \
 	-fdata-sections \
 	-ffunction-sections \
